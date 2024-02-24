ef2ace37df9fd47de19a46104ade06971127399c
Stefano Park Kim <parkkim2@illinois.edu>
2024-02-24 17:23:20 -0600

    autonomus code

--- code/src/brains/autonomous.py
+++ code/src/brains/autonomous.py
@@ -1,7 +1,5 @@
 from . import base
-import cv2
-import cvlib
-from cvlib.object_detection import draw_bbox
+
 
 class Config(base.Config):
     pass
@@ -13,19 +11,10 @@ class Brain(base.Brain):
 
     def __init__(self, config: Config, *arg):
         super().__init__(config, *arg)
-        video = cv2.VideoCapture(0)
 
-    def obj_detection(self):
-        _, frame = self.video.read()
-        box, label, conf = cvlib.detect_common_objects(frame)
-        image = draw_bbox(frame, box, label, conf)
-
-        cv2.imshow("Camera Feed", image)
     def logic(self):
-        
-        self.obj_detection()
         """If anything is detected by the distance_sensors, stop the car"""
-        """
+
         # if anything is detected by the sensors, stop the car
         stop = False
         for distance_sensor in self.distance_sensors:
@@ -34,4 +23,4 @@ class Brain(base.Brain):
                 stop = True
 
         if not stop:
-            self.vehicle.drive_forward()"""
+            self.vehicle.drive_forward()
--- code/test_motor - Copy.py
+++ code/test_motor - Copy.py
@@ -29,9 +29,23 @@ if __name__ == '__main__':
     if -5 <= theta <= 5:
         motor1.forward(speed)
         motor2.forward(speed)
+        time.sleep(5)
+        motor1.stop()
+        motor2.stop()
     elif 5 <= theta < 90:
         motor1.backward(speed)
         motor2.forward(speed)
+        time.sleep(5)
+        motor1.stop()
+        motor2.stop()
     elif -90 < theta < -5:
         motor1.forward(speed)
         motor2.backward(speed)
+        time.sleep(5)
+        motor1.stop()
+        motor2.stop()
+
+motor1.stop()
+motor2.stop()
+        
+        

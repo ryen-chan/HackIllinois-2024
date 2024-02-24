f8034cd52f7bc824580a0b0cd9bd84b819033649
Stefano Park Kim <parkkim2@illinois.edu>
2024-02-24 16:47:49 -0600

    camera working

---
+++ code/cv_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import cv2
+
+from picamera2 import Picamera2
+
+# Grab images as numpy arrays and leave everything else to OpenCV.
+cv2.startWindowThread()
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (320, 240)}))
+picam2.start()
+
+while True:
+    im = cv2.flip(picam2.capture_array(),-1)
+
+    cv2.imshow("cam", im)
+    key = cv2.waitKey(1)
+    
+    if key == 27:
+     break
+
+video.release()
+cv2.destroyAllWindows()
+
+
+  
--- code/test_camera.py
+++ code/test_camera.py
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 from picamera2 import Picamera2, Preview 
 import time 
+=======
+"""
+
+from src import camera as camera_module
+import time
+
+>>>>>>> 7d854a8 (before merge)
 import cv2
 
 cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
@@ -8,6 +16,7 @@ cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
 
+<<<<<<< HEAD
 while cap.isOpened():
     ret, frame = cap.read()
     cv2.imwrite('Atharva_Naik.jpg' , frame)
@@ -16,3 +25,46 @@ while cap.isOpened():
        
 cap.release()
 cv2.destroyAllWindows()  
+=======
+    total_seconds = 60
+    sample_hz = 10
+
+    camera = camera_module.Camera({
+        "show_preview": False
+    })
+
+start_time = time.time()
+
+while time.time() - start_time < total_seconds:
+    camera.capture()
+    print(camera.image_array)
+    #obj_detection()
+    time.sleep(max(0, 1/sample_hz - (time.time() - start_time)))
+"""
+
+from picamera import PiCamera
+from time import sleep
+
+if __name__ == '__main__':
+    camera = PiCamera()
+
+    camera.start_preview()
+    sleep(5)
+    camera.stop_preview()
+"""
+    total_seconds = 60
+    sample_hz = 10
+
+    camera = camera_module.Camera({
+        "show_preview": False
+    })
+    start_time = time.time()
+
+    while time.time() - start_time < total_seconds:
+        camera.capture()
+        print(camera.image_array)
+
+        time.sleep(max(0, 1/sample_hz -
+                       (time.time() - start_time)))
+"""
+>>>>>>> 7d854a8 (before merge)

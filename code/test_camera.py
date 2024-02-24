36a5234a159ad1b84e4e4460a99a8a7c5da150e1
Stefano Park Kim <sparkkim22@student.kis.or.kr>
2024-02-24 16:33:09 -0600

    testing camera with opencv

--- code/test_camera.py
+++ code/test_camera.py
@@ -1,34 +1,18 @@
-from src import camera as camera_module
-from time import sleep
+from picamera2 import Picamera2, Preview 
+import time 
+import cv2
 
-"""
-def obj_detection():
-        _, frame = video.read()
-        box, label, conf = cvlib.detect_common_objects(frame)
-        image = draw_bbox(frame, box, label, conf)
-        cv2.imshow("Camera Feed", image)
-"""
-if __name__ == '__main__':
-    camera_module.start_preview()
-    sleep(5)
-    camera_module.stop_preview()
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 
+#set dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
 
-    """
-    video = cv2.VideoCapture(0)
-
-    total_seconds = 60
-    sample_hz = 10
-
-    camera = camera_module.Camera({
-        "show_preview": False
-    })
-    start_time = time.time()
-
-    while time.time() - start_time < total_seconds:
-        camera.capture()
-        print(camera.image_array)
-        #obj_detection()
-        time.sleep(max(0, 1/sample_hz -
-                       (time.time() - start_time)))
-    """
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imwrite('Atharva_Naik.jpg' , frame)
+    cv2.imshow('Video', frame)
+    cv2.waitKey(0)
+       
+cap.release()
+cv2.destroyAllWindows()  

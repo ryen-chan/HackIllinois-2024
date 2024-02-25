06cd46bd7ee8f4b61ed24d2ca9a395c72c63a55b
Ryan Chen <ryanachen@gmail.com>
2024-02-24 20:33:37 -0600

    Object detection with RPi

--- code/test_detection.py
+++ code/test_detection.py
@@ -1,19 +1,30 @@
 import cv2
 import cvlib
 from cvlib.object_detection import draw_bbox
+from picamera2 import Picamera2
 
+"""
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+"""
 
-video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# Grab images as numpy arrays and leave everything else to OpenCV.
+cv2.startWindowThread()
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (320, 240)}))
+picam2.start()
+
+#video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 def obj_detection():
  
     #frame = cv2.imread('C:\\Users\\ryan\\Desktop\\OpenCV Test\\Cute_dog.jpg')
-    ret, frame = video.read()
-    if not ret:
-        print("Could not read frame")
-        exit()
+    frame = cv2.flip(picam2.capture_array(),-1)
+    #ret, frame = video.read()
+    #if not ret:
+        #print("Could not read frame")
+        #exit()
 
     box, label, conf = cvlib.detect_common_objects(frame, confidence=0.25, model='yolov3-tiny')
     image = draw_bbox(frame, box, label, conf)
@@ -29,5 +40,9 @@ while True:
     if(cv2.waitKey(1) == ord("q")):
         break
         
-video.release()
+picam2.release()
 cv2.destroyAllWindows()
+
+
+
+  
